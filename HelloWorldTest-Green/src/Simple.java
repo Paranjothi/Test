a803c522c17c6df99c3669d26c9aa0c8eabd01d1
Paranjothi <a.paranjothi@gmail.com>
2018-11-13 11:51:32 +0530

    Update Simple.java

--- HelloWorldTest-Green/src/Simple.java
+++ HelloWorldTest-Green/src/Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello !!!!!!! Welcome to CICD Test");  
+     System.out.println("Hello !!!!!!! Welcome to CICD with Green Deployment Test");  
     }  
 }  
